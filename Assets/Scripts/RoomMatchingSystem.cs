5270d5ba12987486b74c916790418a1449f96767
Yyuki78 <yuki.yamada2411@gmail.com>
2022-08-02 15:28:55 +0900

    マッチングシステムを試作

--- Assets/Scripts/RoomMatchingSystem.cs
+++ Assets/Scripts/RoomMatchingSystem.cs
@@ -8,113 +8,305 @@ using Photon.Realtime;
 
 public class RoomMatchingSystem : MonoBehaviourPunCallbacks
 {
-    private RoomList roomList = new RoomList();
-    private List<RoomButton> roomButtonList = new List<RoomButton>();
-    private CanvasGroup canvasGroup;
+    // 最大人数
+    [SerializeField] private int maxPlayers = 5;
 
-    //Playerの番号
-    public static int PlayerNum = 0;
-    public static int PlayerNum2 = 0;
-    private int PlayerCount = 0;
+    // 公開・非公開
+    [SerializeField] private bool isVisible = true;
 
-    IconDisplay _iconDisplay;
+    // 入室の可否
+    [SerializeField] private bool isOpen = true;
 
-    [SerializeField]
-    Text joinedMembersText;
+    // 部屋名
+    [SerializeField] private string roomName1 = "Room1";
+    [SerializeField] private string roomName2 = "Room2";
 
-    [SerializeField]
-    GameObject ReadyButton;
 
+    /////////////////////////////////////////////////////////////////////////////////////
+    // Awake & Start ////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+
+    // Awake
+    private void Awake()
+    {
+        // シーンの自動同期: 無効
+        PhotonNetwork.AutomaticallySyncScene = false;
+    }
+
+
+    // Start is called before the first frame update
     private void Start()
     {
-        canvasGroup = GetComponent<CanvasGroup>();
-        _iconDisplay = GetComponent<IconDisplay>();
-        // ロビーに参加するまでは、全てのルーム参加ボタンを押せないようにする
-        canvasGroup.interactable = false;
-
-        // 全てのルーム参加ボタンを初期化する
-        int roomId = 1;
-        foreach (Transform child in transform)
+        // Photonに接続
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    /*
+    // ニックネームを付ける
+    private void SetMyNickName(string nickName)
+    {
+        if (PhotonNetwork.IsConnected)
         {
-            if (child.TryGetComponent<RoomButton>(out var roomButton))
-            {
-                //////////////roomButton.Init(this, roomId++);
-                roomButtonList.Add(roomButton);
-            }
+            PhotonNetwork.LocalPlayer.NickName = nickName;
+        }
+    }*/
+
+    // ロビーに入る
+    private void JoinLobby()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinLobby();
         }
     }
 
+    //家のボタン(左側)
+    //部屋に入室する （存在しなければ作成して入室する）
+    public void JoinOrCreateRoom1()
+    {
+        // ルームオプションの基本設定
+        RoomOptions roomOptions = new RoomOptions
+        {
+            // 部屋の最大人数
+            MaxPlayers = (byte)maxPlayers,
+
+            // 公開
+            IsVisible = isVisible,
+
+            // 入室可
+            IsOpen = isOpen
+        };
+
+        // 入室 (存在しなければ部屋を作成して入室する)
+        if (PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinOrCreateRoom(roomName1, roomOptions, TypedLobby.Default);
+        }
+    }
+
+    //家のボタン(右側)
+    //部屋に入室する （存在しなければ作成して入室する）
+    public void JoinOrCreateRoom2()
+    {
+        // ルームオプションの基本設定
+        RoomOptions roomOptions = new RoomOptions
+        {
+            // 部屋の最大人数
+            MaxPlayers = (byte)maxPlayers,
+
+            // 公開
+            IsVisible = isVisible,
+
+            // 入室可
+            IsOpen = isOpen
+        };
+
+        // 入室 (存在しなければ部屋を作成して入室する)
+        if (PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinOrCreateRoom(roomName2, roomOptions, TypedLobby.Default);
+        }
+    }
+
+    // 部屋から退室する
+    public void LeaveRoom()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            // 退室
+            PhotonNetwork.LeaveRoom();
+        }
+    }
+
+    // Photonに接続した時
+    public override void OnConnected()
+    {
+        Debug.Log("OnConnected");
+
+        // ニックネームを付ける
+        //SetMyNickName("Knohhoso");
+    }
+
+
+    // Photonから切断された時
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnDisconnected");
+    }
+
+
+    // マスターサーバーに接続した時
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster");
+
+        // ロビーに入る
+        JoinLobby();
+    }
+
+
+    // ロビーに入った時
     public override void OnJoinedLobby()
     {
-        // ロビーに参加したら、ルーム参加ボタンを押せるようにする
-        canvasGroup.interactable = true;
+        Debug.Log("OnJoinedLobby");
     }
 
-    public override void OnRoomListUpdate(List<RoomInfo> changedRoomList)
+
+    // ロビーから出た時
+    public override void OnLeftLobby()
     {
-        roomList.Update(changedRoomList);
-
-        // 全てのルーム参加ボタンの表示を更新する
-        foreach (var roomButton in roomButtonList)
-        {
-            if (roomList.TryGetRoomInfo(roomButton.RoomName, out var roomInfo))
-            {
-                roomButton.SetPlayerCount(roomInfo.PlayerCount);
-            }
-            else
-            {
-                roomButton.SetPlayerCount(0);
-            }
-        }
+        Debug.Log("OnLeftLobby");
     }
 
-    public void OnJoiningRoom()
+
+    // 部屋を作成した時
+    public override void OnCreatedRoom()
     {
-        // ルーム参加処理中は、全てのルーム参加ボタンを押せないようにする
-        canvasGroup.interactable = false;
+        Debug.Log("OnCreatedRoom");
     }
 
-    //Roomに入った
+
+    // 部屋の作成に失敗した時
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnCreateRoomFailed");
+    }
+
+
+    // 部屋に入室した時
     public override void OnJoinedRoom()
     {
-        GameManager.Instance.SetCurrentState(GameManager.GameMode.InRoom);
-        Debug.Log(PhotonNetwork.NickName + " is joined.");
+        Debug.Log("OnJoinedRoom");
 
-        //自分の番号を取得する
-        PlayerNum2 = PhotonNetwork.PlayerList.Length;
+        //プレーヤーのカスタムプロパティ更新
+        SetMyCustomProperties();
 
-        UpdateMemberList();
-        _iconDisplay.UpdateLabel();
-        // ルームへの参加が成功したら、UIを非表示にする
-        //gameObject.SetActive(false);
+        /*
+        // 部屋の情報を表示
+        if (PhotonNetwork.InRoom)
+        {
+            Debug.Log("RoomName: " + PhotonNetwork.CurrentRoom.Name);
+            Debug.Log("HostName: " + PhotonNetwork.MasterClient.NickName);
+            Debug.Log("Stage: " + PhotonNetwork.CurrentRoom.CustomProperties["Stage"] as string);
+            Debug.Log("Difficulty: " + PhotonNetwork.CurrentRoom.CustomProperties["Difficulty"] as string);
+            Debug.Log("Slots: " + PhotonNetwork.CurrentRoom.PlayerCount + " / " + PhotonNetwork.CurrentRoom.MaxPlayers);
+        }*/
     }
 
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        Debug.Log(PhotonNetwork.NickName + " is joined.");
-        UpdateMemberList();
-        _iconDisplay.UpdateLabel();
-    }
 
+    // 特定の部屋への入室に失敗した時
     public override void OnJoinRoomFailed(short returnCode, string message)
     {
-        // ルームへの参加が失敗したら、再びルーム参加ボタンを押せるようにする
-        canvasGroup.interactable = true;
+        Debug.Log("OnJoinRoomFailed");
     }
 
-    public void UpdateMemberList()
+
+    // ランダムな部屋への入室に失敗した時
+    public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        PlayerCount = 0;
-        joinedMembersText.text = "";
-        foreach (var p in PhotonNetwork.PlayerList)
+        Debug.Log("OnJoinRandomFailed");
+    }
+
+
+    // 部屋から退室した時
+    public override void OnLeftRoom()
+    {
+        Debug.Log("OnLeftRoom");
+    }
+
+
+    // 他のプレイヤーが入室してきた時
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("OnPlayerEnteredRoom");
+    }
+
+
+    // 他のプレイヤーが退室した時
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log("OnPlayerLeftRoom");
+    }
+
+
+    // マスタークライアントが変わった時
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        Debug.Log("OnMasterClientSwitched");
+    }
+
+
+    // ロビーに更新があった時
+    public override void OnLobbyStatisticsUpdate(List<TypedLobbyInfo> lobbyStatistics)
+    {
+        Debug.Log("OnLobbyStatisticsUpdate");
+    }
+
+
+    // ルームリストに更新があった時
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        Debug.Log("OnRoomListUpdate");
+    }
+
+
+    // ルームプロパティが更新された時
+    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    {
+        Debug.Log("OnRoomPropertiesUpdate");
+    }
+
+
+    // プレイヤープロパティが更新された時
+    public override void OnPlayerPropertiesUpdate(Player target, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        Debug.Log("OnPlayerPropertiesUpdate");
+    }
+
+    /// <summary>
+    /// プレイヤーに番号を与える
+    /// </summary>
+    private void SetMyCustomProperties()
+    {
+        //自分のクライアントの同期オブジェクトにのみ
+        if (photonView.IsMine)
         {
-            PlayerCount++;
-            joinedMembersText.text += PhotonNetwork.NickName + "\n";
-        }
-        if (PlayerCount == 5)
-        {
-            Debug.Log("5人が揃いました");
-            ReadyButton.SetActive(true);
+            List<int> playerSetableCountList = new List<int>();
+
+            //制限人数までの数字のリストを作成
+            //例) 制限人数 = 4 の場合、{0,1,2,3}
+            int count = 0;
+            for (int i = 0; i < maxPlayers; i++)
+            {
+                playerSetableCountList.Add(count);
+                count++;
+            }
+
+            //他の全プレイヤー取得
+            Player[] otherPlayers = PhotonNetwork.PlayerListOthers;
+
+            //他のプレイヤーがいなければカスタムプロパティの値を"0"に設定
+            if (otherPlayers.Length <= 0)
+            {
+                //ローカルのプレイヤーのカスタムプロパティを設定
+                int playerAssignNum = otherPlayers.Length;
+                PhotonNetwork.LocalPlayer.UpdatePlayerNum(playerAssignNum);
+                return;
+            }
+
+            //他のプレイヤーのカスタムプロパティー取得してリスト作成
+            List<int> playerAssignNums = new List<int>();
+            for (int i = 0; i < otherPlayers.Length; i++)
+            {
+                playerAssignNums.Add(otherPlayers[i].GetPlayerNum());
+            }
+
+            //リスト同士を比較し、未使用の数字のリストを作成
+            //例) 0,1にプレーヤーが存在する場合、返すリストは2,3
+            playerSetableCountList.RemoveAll(playerAssignNums.Contains);
+
+            //ローカルのプレイヤーのカスタムプロパティを設定
+            //空いている場所のうち、一番若い数字の箇所を利用
+            PhotonNetwork.LocalPlayer.UpdatePlayerNum(playerSetableCountList[0]);
         }
     }
 }
