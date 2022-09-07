cb7157023344a0ac84487d16f7437436cd282b79
kotaro45d <kotaro45d@gmail.com>
2022-09-07 18:51:19 +0900

    no message

---
+++ Assets/Uchiyama/CountdownAnimation.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownAnimation : MonoBehaviour
+{
+
+    /*[SerializeField]
+    Image countdown;
+
+    [SerializeField]
+    Text countdownText;
+
+    int countDownCount;
+
+    float countDownElapsedTime;
+
+    float countDownDuration = 3.0f;*/
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+}
